6495f799cd669dad0260186a5f57573df2fcd553
Heinrich <dheinrich@HEIDA-AMBP2.fritz.box>
2020-03-27 16:21:12 +0100

    different cases

--- src/test/java/data/empty/asso/MyEntityRepositoryTest.java
+++ src/test/java/data/empty/asso/MyEntityRepositoryTest.java
@@ -4,7 +4,9 @@ import data.empty.asso.buggy.MyBEntity;
 import data.empty.asso.buggy.MyBEntityRepository;
 import data.empty.asso.working.MyEntity;
 import data.empty.asso.working.MyEntityRepository;
+import data.empty.asso.working.MyOther;
 import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
@@ -14,15 +16,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @MicronautTest
 class MyEntityRepositoryTest {
 
+
     @Inject
     MyEntityRepository repository;
 
-    @Inject
-    MyBEntityRepository brepository;
-
     @Test
-    void working() {
-        var ent = repository.save(new MyEntity(-1, null));
+    void emptyRelation() {
+        var ent = repository.save(new MyEntity(-1));
         assertFalse(ent.getLid() == -1);
 
         var found = repository.findById(ent.getLid()).get();
@@ -31,7 +31,22 @@ class MyEntityRepositoryTest {
     }
 
     @Test
-    void buggy() {
+    void nonemptyRelation() {
+        var ent = repository.save(new MyEntity(-1, new MyOther("foo")));
+        assertFalse(ent.getLid() == -1);
+
+        var found = repository.findById(ent.getLid()).get();
+        assertNotNull(found.getOther());
+        assertEquals(found.getOther().getLid(), "foo");
+
+        repository.update(found);
+    }
+
+    @Inject
+    MyBEntityRepository brepository;
+
+    @Test
+    void emptyRelation2() {
         var ent = brepository.save(new MyBEntity(-1, null));
         assertFalse(ent.getLid() == -1);
 
